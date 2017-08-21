a2ccbe42f72c998fc149019c129c83f295bd4588
magyk81 <magyk81@gmail.com>
2017-08-20 19:17:05 -0600

    got nothing done

--- Assets/Scripts/FlowerScript.cs
+++ Assets/Scripts/FlowerScript.cs
@@ -5,17 +5,17 @@ using UnityEngine.UI;
 
 public class FlowerScript : MonoBehaviour {
 
-    //public VideoClip[] movies;
-    private MovieTexture[] movieTextures;
+    //public VideoClip movie;
+    public Texture movieTexture;
     public GameObject gameObject;
 
 	// Use this for initialization
 	void Start ()
     {
-        movies[0] = new MovieTexture();
+        movieTexture = new MovieTexture();
         //gameObject.GetComponent<MeshRenderer>();
         //gameObject.GetComponent<Material>().GetTexture = movie as MovieTexture;
-        gameObject.GetComponent<Renderer>().GetComponent<Material>().mainTexture = movies[0] as MovieTexture;
+        gameObject.GetComponent<Renderer>().GetComponent<Material>().mainTexture = movieTexture as MovieTexture;
 
         // for testing
         //movie.Play();
